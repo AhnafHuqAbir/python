b4613c2c781377fe0f9a4806a46ee32c62edf2c6
AhanfHuqAbir <ahnafhuqabir@gmail.com>
2025-05-23 22:40:45 -0400

    acp completed

---
+++ Lesson 3/lesson3acp.py
@@ -0,0 +1,12 @@
+# congratulation massage
+name = input("What is your name :")
+print("Hi", name,)
+
+today = input("Today is your birthday (yes/no):")
+if today == "yes" :
+    print(" \n 💫 congratulation 💫 \n Happy birthday ", name)
+else :
+    deat = input("What is your birthday!!! :")
+    work = input(name, "What do you want to do that day!!! :")
+    print("Okay,", work, "what you love on", deat,".Good luck.")
+    
