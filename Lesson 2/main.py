1cd44c589190b30261c5cf64d0cb43b58c8425f4
AhanfHuqAbir <ahnafhuqabir@gmail.com>
2025-05-29 00:42:42 -0400

    done

---
+++ Lesson 2/main.py
@@ -0,0 +1,19 @@
+# Muntakim Rhaman
+muntakim_birthday = "12th june 2011"
+print("Muntakim DOB :", muntakim_birthday)
+
+# Partho Roy
+partho_birthday = "1st january 2010"
+print("Partho DOB :", partho_birthday)
+
+# Rahi birthday
+rahiBirthday = "29th February 2012"
+print("Rahi birthday :", rahiBirthday)
+
+# Rosni Mollika
+rosni = "7th July 2011"
+print("Rosni DOB :", rosni)
+
+# Kona Rhaman 
+kona = "3th December 2010"
+print("Kona DOB :", kona)
