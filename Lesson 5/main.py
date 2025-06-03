b809bcfb190795a25c9aa12d582adec685cc11f8
AhanfHuqAbir <ahnafhuqabir@gmail.com>
2025-06-03 17:17:37 -0400

    completed acps

---
+++ Lesson 5/main.py
@@ -0,0 +1,8 @@
+x = int(input("What is your temperatura there : "))
+
+if x > 25:
+    print(" 🥵 It's summer time 🥵 ")
+elif x >15:
+    print(" 😸 It's milder time 😸")
+else:
+    print("☃️ It's winter time ☃️ ")
