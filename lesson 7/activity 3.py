612bde2d3e39afe77414f7aa8c96c47bae325abc
AhanfHuqAbir <ahnafhuqabir@gmail.com>
2025-06-24 15:18:17 -0400

    ACP Completed

--- lesson 7/activity 3.py
+++ lesson 7/activity 3.py
@@ -1,1 +1,30 @@
 # Write a program to select a ride according to your preference. The ride is divided into two major categories: 1. Bike 2. Car And further, bikes and cars are divided into 2 subcategories. To give the user better selection options.
+
+print("Select your ride")
+print("1. bike")
+print("2. car")
+
+choice = int(input("Enter your choice : "))
+
+if choice == 1:
+    print("What type of bike : ")
+    print("1.Scooty \n")
+    print("2.Scooter \n")
+    choice2 = int(input("Enter your choice : "))
+    if choice2 == 1:
+     print("you have selected scooty")
+    elif choice2 == 2:
+       print("you have selected scooter")
+    else :
+       print("please choice some of them 1 or 2")
+elif choice == 2:
+    print("what type of car : ")
+    print("1.Sedan")
+    print("2.xuv") 
+    choice3 = int(input("Enter your choice 1 or 2 : "))
+    if choice3 == 1:
+        print("you have choiced sedan")
+    elif choice3 == 2:
+       print("you have choiced XUV")
+    else :
+       print("Enter your choice 1 or 2") 
---
+++ lesson 7/main.py
@@ -0,0 +1,6 @@
+# name = input("Hi! what is your name : ")
+age = int(input("How old are you (Enter in number) : "))
+if age > 10 :
+    print("You are above 10 years old!!!")
+else :
+    print("you are under 10 years old")
