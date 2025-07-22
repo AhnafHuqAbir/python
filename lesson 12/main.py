fa998fb8f212a0e9909911800a07bf79fa4b1124
AhanfHuqAbir <ahnafhuqabir@gmail.com>
2025-07-22 15:39:28 -0400

    New acp done

--- lesson 12/main.py
+++ lesson 12/main.py
@@ -2,11 +2,14 @@ import turtle
 screen = turtle.Screen()
 screen.bgcolor("dark blue")
 screen.title("Multi squier")
+screen.setup(800,800)
 pen = turtle.Turtle()
+pen.shape("turtle")
+pen.speed(7)
 size = 5
 while turtle:
     for x in range(4):
         pen.fd(size + 1)
-        pen.left(90.1)
+        pen.left(270)
         size = size - 5
     size = size + 1
